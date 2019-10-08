8417fea141764638f636353579edb077dec4e2b2
abeken1011 <abekensuke.1011@gmail.com>
2019-10-08 16:39:45 +0900

    fix some bag and change template

--- app.py
+++ app.py
@@ -8,15 +8,15 @@ app.debug = True
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html", title="aa", message="fuck")
+    return render_template("index.html", message="最初のメッセージを入力してね！")
 
 @app.route("/", methods=["POST"])
 def form():
     with open('model.pkl', 'rb') as f:
         model = cloudpickle.load(f)
     field=request.form["field"]
-    maked_words = LSTM_model.generate_seq(model, start_phase=field, length=10)
-    return render_template("index.html", title="Index with Jinja", message="Hello" + maked_words)
+    maked_words = LSTM_model.generate_seq(model, start_phase=field, length=20)
+    return render_template("index.html", message=maked_words)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
